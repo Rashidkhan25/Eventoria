e0c950148750659893cd27b6e36cc0837cf3d084
Vaidehi <vaidehiraut21@gmail.com>
2025-10-26 09:13:07 -0700

    changes

--- app.js
+++ app.js
@@ -14,12 +14,11 @@ const {
 } = require("./data/eventsData");
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 
 // Session middleware
 app.use(session({
